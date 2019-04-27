fdd68725eadba86aeda7c6d2a1a1a360a33b0b6f
Omar Bishtawi <omar@dawsat.com>
2019-04-27 20:17:43 +0300

    fix client password reset

--- app/Http/Controllers/Client/ResetPasswordController.php
+++ app/Http/Controllers/Client/ResetPasswordController.php
@@ -58,7 +58,7 @@ class ResetPasswordController extends Controller
         // Here we will attempt to reset the user's password. If it is successful we
         // will update the password on an actual user model and persist it to the
         // database. Otherwise we will parse the error and return the response.
-        $user = Client::find(1);
+        $user = Client::where('email', $request->input('email'))->first();
         $password = $request->password;
         $response = Password::reset(
             $this->credentials($request), function ($user, $password) {
