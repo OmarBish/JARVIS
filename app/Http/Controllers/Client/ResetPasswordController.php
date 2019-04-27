1117eb9ce38c0936cf6ad8c8201cc45f2440f939
Omar Bishtawi <omar@dawsat.com>
2019-04-27 19:53:54 +0300

    update reset password class

--- app/Http/Controllers/Client/ResetPasswordController.php
+++ app/Http/Controllers/Client/ResetPasswordController.php
@@ -67,11 +67,9 @@ class ResetPasswordController extends Controller
         );
        
 
-        // If the password was successfully reset, we will redirect the user back to
-        // the application's home authenticated view. If there is an error we can
-        // redirect them back to where they came from with their error message.
+        
         return $response == Password::PASSWORD_RESET
                     ? $this->sendResponse('', 'password successfuly changed')
-                    : $this->sendError('invalid token', ''); 
+                    : $this->sendError('invalid token', trans($response)); 
     }
 }
